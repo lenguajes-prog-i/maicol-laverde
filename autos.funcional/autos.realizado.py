fff6faf4ef8a3fcceb50d87647fe87fc773303ab
ingenierolaverde24-oss <ingeniero.laverde24@gmail.com>
2026-04-14 22:39:10 -0500

    codigo ya realizado con sus respectivos cambios

---
+++ autos.funcional/autos.realizado.py
@@ -0,0 +1,32 @@
+import pickle
+
+def crear_auto(modelo, placa):
+    return {"modelo": modelo, "placa": placa}
+ 
+def representar_auto(auto):
+    return f"El auto {auto['modelo']} tiene placa {auto['placa']}"
+
+autos = [
+
+crear_auto("BMW", "IBC123"),
+
+crear_auto("toyota", "HCM123"),
+
+crear_auto("Ferrari", "BLB123"),
+
+crear_auto ("mercedes benz", "ELM121"),
+
+crear_auto("Lamborghini veneno", "REA987")
+
+]
+
+with open("autos.txt", "wb") as archivo:
+    pickle.dump(autos, archivo)
+
+#Leer archivo
+
+with open("autos.txt", "rb") as archivo:
+    autos_cargados = pickle.load(archivo)
+
+
+list(map(lambda auto: print(representar_auto(auto)), autos_cargados))
